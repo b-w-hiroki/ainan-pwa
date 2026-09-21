a689f7dacc15e3baee36ad0aa0f4c0b57d962c96
B.W-Hiroki <kojack1221@gmail.com>
2026-09-21 10:32:29 +0900

    feat: centralize boss visual sizes and accents

--- fishing-game/js/game/bossArt.js
+++ fishing-game/js/game/bossArt.js
@@ -1,11 +1,13 @@
 import { ASSETS } from '../config/assetManifest.js'
 import { BOSS_META } from './midgameProgression.js'
 
-export const BOSS_ART = {
-  harborRunner: ASSETS.bosses.harborRunner,
-  bayHunter: ASSETS.bosses.bayHunter,
-  kue: ASSETS.bosses.kue,
+export const BOSS_VISUALS = {
+  harborRunner: { asset: ASSETS.bosses.harborRunner, accent: 0x5bb5d8, secondary: 0xffd95a, battle: [236, 144], result: [238, 146] },
+  bayHunter: { asset: ASSETS.bosses.bayHunter, accent: 0x8f80e8, secondary: 0x71d6a2, battle: [244, 149], result: [246, 150] },
+  kue: { asset: ASSETS.bosses.kue, accent: 0xff765a, secondary: 0xffd95a, battle: [278, 172], result: [270, 167] },
 }
+
+export const BOSS_ART = Object.fromEntries(Object.entries(BOSS_VISUALS).map(([id, visual]) => [id, visual.asset]))
 
 export function getBossMetaForScene(scene) {
   return Object.values(BOSS_META).find(meta =>
@@ -17,7 +19,15 @@ export function getBossArt(id) {
   return BOSS_ART[id] ?? null
 }
 
+export function getBossVisual(id) {
+  return BOSS_VISUALS[id] ?? null
+}
+
 export function getBossArtForScene(scene) {
   const meta = getBossMetaForScene(scene)
   return meta ? getBossArt(meta.id) : null
+}
+
+export function getAllBossAssets() {
+  return Object.values(BOSS_VISUALS).map(item => item.asset)
 }
