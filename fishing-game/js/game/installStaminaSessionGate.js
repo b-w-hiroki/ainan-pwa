41e80beec9ed171e02141fbb99d3919993ec9028
B.W-Hiroki <kojack1221@gmail.com>
2026-09-24 21:52:44 +0900

    test: pin canonical battle fish above final presentation

--- fishing-game/js/game/installStaminaSessionGate.js
+++ fishing-game/js/game/installStaminaSessionGate.js
@@ -86,26 +86,17 @@ function stabilizeMockBattle(scene) {
   }
 
   scene._qaMockBattleFish?.destroy?.()
-  const targetTexture = target?._assetImage?.texture?.key
-  const shadowKey = ASSETS.fishingField?.fishShadowMediumIdle?.key
-  const qaTexture = targetTexture && scene.textures?.exists?.(targetTexture)
-    ? targetTexture
-    : shadowKey && scene.textures?.exists?.(shadowKey)
-      ? shadowKey
-      : scene.textures?.exists?.('fish_aji_icon') ? 'fish_aji_icon' : null
-
-  if (qaTexture) {
-    const isIcon = qaTexture === 'fish_aji_icon'
-    scene._qaMockBattleFish = scene.add.image(scene.scale.width / 2, 330, qaTexture)
-      .setDisplaySize(isIcon ? 156 : 176, isIcon ? 156 : 88)
-      .setDepth(500)
+  if (scene.textures?.exists?.('fish_aji_icon')) {
+    scene._qaMockBattleFish = scene.add.image(scene.scale.width / 2, 345, 'fish_aji_icon')
+      .setDisplaySize(172, 132)
+      .setDepth(900)
       .setScrollFactor(0)
-      .setAlpha(0.98)
+      .setAlpha(1)
   } else {
-    const g = scene.add.graphics().setDepth(500).setScrollFactor(0)
-    g.fillStyle(0x0b3046, 0.92)
-    g.fillEllipse(scene.scale.width / 2, 330, 168, 76)
-    g.fillTriangle(scene.scale.width / 2 + 70, 330, scene.scale.width / 2 + 112, 298, scene.scale.width / 2 + 112, 362)
+    const g = scene.add.graphics().setDepth(900).setScrollFactor(0)
+    g.fillStyle(0x0b3046, 0.96)
+    g.fillEllipse(scene.scale.width / 2, 345, 168, 76)
+    g.fillTriangle(scene.scale.width / 2 + 70, 345, scene.scale.width / 2 + 112, 313, scene.scale.width / 2 + 112, 377)
     scene._qaMockBattleFish = g
   }
 }
