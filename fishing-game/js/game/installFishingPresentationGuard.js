09b9114477517046e0997d94034a7cb23577e1a5
B.W-Hiroki <kojack1221@gmail.com>
2026-09-25 23:02:16 +0900

    polish: show pier decor in cast and retrieve

--- fishing-game/js/game/installFishingPresentationGuard.js
+++ fishing-game/js/game/installFishingPresentationGuard.js
@@ -284,7 +284,7 @@ function applyPhasePresentation(scene, phase = scene.phase) {
   hideLegacyGuideChrome(scene)
   syncMockFieldStaging(scene, phase)
   const decor = buildLeftPierDecor(scene)
-  decor?.setVisible?.(false)
+  decor?.setVisible?.(cast || retrieve)
   const playerHero = buildRcPlayerHero(scene)
   playerHero?.setVisible?.(cast || retrieve)
   scene._blueprintCastInstruction?.setVisible?.(cast)
